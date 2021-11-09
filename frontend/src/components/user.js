c42493379225f2a8877ab3dc20f0663c351950e4
ArianeValasques <ariane.souza288@gmail.com>
2021-11-08 23:01:20 -0300

    Correções

--- frontend/src/components/user.js
+++ frontend/src/components/user.js
@@ -1,8 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Context } from '../context/authContext';
 import '../styles/components/user.css';
-import { FaUser , FaSignOutAlt} from 'react-icons/fa';
+import { FaSignOutAlt} from 'react-icons/fa';
+import api from '../services/api';
+import Avatar from 'react-avatar';
+import AvatarEditor from 'react-avatar-editor';
 
 export default function User(){
 
@@ -11,15 +14,40 @@ export default function User(){
     const sobrenome = localStorage.getItem('sobrenome');
     const id = localStorage.getItem('id');
 
+    const initUsuario = {
+        email: '',
+        senha: '',
+        nome: '',
+        sobrenome: '',
+        telefone: '',
+        funcao: '',
+        dataCadastro: '',
+        imagem: '',
+        status: 'Ativo'
+    }
+    const [user, setUser] = useState(initUsuario);
+
+    useEffect(() => {
+        if (id) {
+            api.get(`/users/profile/${id}`).then(response => {
+                setUser(...response.data)
+            })
+        }
+    }, []);
+
+    var imagem = user.imagem;
+    console.log(imagem)
+
     return(
-       
         <user>
             <Link to={`/EditarUsuario/${id}`} className="user-button">
+                <AvatarEditor width={40} height={40} border={10} borderRadius={20} src ={imagem}>
+                    <img src ={imagem} alt = "" />
+                </AvatarEditor>
                 {nome + ' ' + sobrenome}</Link>
             <button className="user-logout" type="button" onClick={handleLogout}>
                 Sair<icon><FaSignOutAlt/></icon>
             </button>
         </user>
-            
     ); 
 }
--- frontend/src/pages/EditarUsuario/editarUsuario.js
+++ frontend/src/pages/EditarUsuario/editarUsuario.js
@@ -24,7 +24,7 @@ export default function EditarUsuario() {
         sobrenome: '',
         telefone: '',
         funcao: '',
-        dataCadastro: date.getDate(),
+        dataCadastro: '',
         imagem: '',
         status: 'Ativo'
     }
